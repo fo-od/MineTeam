46eab74996826a0f8e4f65fa1db0783411d76839
fo-od <fooduh@proton.me>
2026-07-31 01:58:04 -0700

    start work on radial menu

---
+++ src/main/java/dev/fooduhhh/craft_team/client/CraftTeamClient.java
@@ -0,0 +1,25 @@
+package dev.fooduhhh.craft_team.client;
+
+import dev.fooduhhh.craft_team.client.actions.MenuHandler;
+import dev.fooduhhh.craft_team.client.actions.MenuRender;
+import dev.fooduhhh.craft_team.common.Constants;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
+
+@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.GAME)
+public class CraftTeamClient {
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+        MenuHandler.onClientTick(event);
+    }
+
+    @SubscribeEvent
+    public static void onRenderGui(RenderGuiEvent.Post event) {
+        if (MenuHandler.isMenuOpen) {
+            MenuRender.render(event.getGuiGraphics());
+        }
+    }
+}
--- src/main/java/dev/fooduhhh/craft_team/client/gui/actions/MenuRender.java
+++
@@ -1,4 +0,0 @@
-package dev.fooduhhh.craft_team.client.gui.actions;
-
-public class MenuRender {
-}
