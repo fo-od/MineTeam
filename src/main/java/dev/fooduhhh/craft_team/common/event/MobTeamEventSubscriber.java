c2cca9f508eb2493abc6579e91c570cedf498195
fo-od <fooduh@proton.me>
2026-08-01 23:47:14 -0700

    refactor mob taming

--- src/main/java/dev/fooduhhh/craft_team/common/event/MobTeamEventSubscriber.java
+++ src/main/java/dev/fooduhhh/craft_team/common/event/MobTeamEventSubscriber.java
@@ -3,7 +3,6 @@ package dev.fooduhhh.craft_team.common.event;
 import dev.fooduhhh.craft_team.common.Constants;
 import dev.fooduhhh.craft_team.common.config.CraftTeamConfig;
 import dev.fooduhhh.craft_team.common.network.MobTamingS2CPayload;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.ServerScoreboard;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
@@ -19,71 +18,45 @@ import net.minecraft.world.scores.PlayerTeam;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.neoforge.event.tick.EntityTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
-
-import java.util.Objects;
 
 @EventBusSubscriber(modid = Constants.MOD_ID)
 public class MobTeamEventSubscriber {
+
     @SubscribeEvent
     public static void onPlayerInteractEntityInteract(PlayerInteractEvent.EntityInteract event) {
         Level level = event.getLevel();
         InteractionHand hand = event.getHand();
         Player player = event.getEntity();
 
-        if (level.isClientSide() || hand != InteractionHand.MAIN_HAND) {
-            return;
-        }
+        if (level.isClientSide() || hand != InteractionHand.MAIN_HAND) return;
 
-        ItemStack itemstack = player.getItemInHand(hand);
         Entity target = event.getTarget();
         Ingredient tamingMaterial = CraftTeamConfig.getTamingMaterial(target.getType());
-        CompoundTag tag = target.getPersistentData();
+        ItemStack itemstack = player.getItemInHand(hand);
 
-        if (tamingMaterial.test(itemstack) && target instanceof LivingEntity livingEntity && !tag.contains("teamTamingTime")) {
+        if (!tamingMaterial.test(itemstack)) return;
+
+        if (target instanceof LivingEntity livingEntity) {
             ServerLevel serverLevel = (ServerLevel) level;
-            PlayerTeam playersTeam = serverLevel.getServer().getScoreboard().getPlayersTeam(target.getScoreboardName());
+            ServerScoreboard scoreboard = serverLevel.getServer().getScoreboard();
 
-            if (livingEntity.hasEffect(MobEffects.WEAKNESS) && playersTeam == null) {
+            PlayerTeam playersTeam = scoreboard.getPlayersTeam(player.getScoreboardName());
+            PlayerTeam targetTeam = scoreboard.getPlayersTeam(livingEntity.getStringUUID());
+
+            if (playersTeam == null || targetTeam != null) return;
+
+            if (livingEntity.hasEffect(MobEffects.WEAKNESS)) {
                 itemstack.consume(1, player);
-                livingEntity.getPersistentData().putInt("TeamConversionTime", livingEntity.level().random.nextInt(2401) + 3600);
-                tag.putString("teamTamingColor", player.getPersistentData().getString("teamColor"));
 
                 livingEntity.removeEffect(MobEffects.WEAKNESS);
                 livingEntity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, Math.min(livingEntity.level().getDifficulty().getId() - 1, 0)));
                 livingEntity.setGlowingTag(true);
 
-                String teamColor = tag.getString("teamTamingColor");
-                ServerScoreboard scoreboard = serverLevel.getServer().getScoreboard();
-                scoreboard.addPlayerToTeam(livingEntity.getScoreboardName(), Objects.requireNonNull(scoreboard.getPlayerTeam(teamColor)));
+                scoreboard.addPlayerToTeam(livingEntity.getStringUUID(), playersTeam);
+                livingEntity.getPersistentData().putString("owner", player.getStringUUID());
+
                 PacketDistributor.sendToPlayersInDimension(serverLevel, new MobTamingS2CPayload(target.getId(), target.blockPosition()));
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public static void onEntityTick(EntityTickEvent.Pre event) {
-        Entity entity = event.getEntity();
-        if (entity instanceof LivingEntity livingEntity && !livingEntity.level().isClientSide) {
-            CompoundTag tag = livingEntity.getPersistentData();
-            if (!tag.contains("teamTamingTime")) {
-                return;
-            }
-            int time = tag.getInt("teamTamingTime");
-            if (time <= 0) {
-                String teamColor = tag.getString("teamTamingColor");
-                ServerLevel serverLevel = (ServerLevel) livingEntity.level();
-                ServerScoreboard scoreboard = serverLevel.getServer().getScoreboard();
-
-                scoreboard.addPlayerToTeam(livingEntity.getScoreboardName(), Objects.requireNonNull(scoreboard.getPlayerTeam(teamColor)));
-
-                livingEntity.setGlowingTag(true);
-
-                tag.remove("teamTamingTime");
-                tag.remove("teamTamingColor");
-            } else {
-                tag.putInt("teamTamingTime", time - 1);
             }
         }
     }
